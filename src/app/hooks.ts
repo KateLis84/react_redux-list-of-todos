841ac10aac3495f84a5a31f1306fedec3465239d
kateliaskov <58365756+KateLis84@users.noreply.github.com>
2025-07-20 20:34:34 +0300

    fix review

--- src/app/hooks.ts
+++ src/app/hooks.ts
@@ -1,5 +1,9 @@
-import { useDispatch, useSelector } from 'react-redux';
+import {
+  TypedUseSelectorHook,
+  useDispatch,
+  useSelector,
+} from 'react-redux';
 import type { RootState, AppDispatch } from './store';
 
 export const useAppDispatch: () => AppDispatch = useDispatch;
-export const useAppSelector = useSelector.withTypes<RootState>();
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
